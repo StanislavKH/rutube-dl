0fd55f098496cf76b7d20f80d6c81a65473dc48b
Stanislav Kharytonov <stanislav@noia.network>
2024-10-10 22:42:19 +0300

    feat: links

--- cmd/rutube-dl/main.go
+++ cmd/rutube-dl/main.go
@@ -8,15 +8,19 @@ import (
 
 func main() {
 	urls := []string{
-		"https://rutube.ru/video/707bb0163d5eafb21be3c4c36ccb3212/", // 101
-		"https://rutube.ru/video/72c9a7faba41d4ddb9b936de430bdf9b/", // 102
-		"https://rutube.ru/video/7340cfc976fd32504496113ec04575b3/", // 103
-		"https://rutube.ru/video/9185cf1ad9d95c578ff5f494e20778ea/", // 104
-		"https://rutube.ru/video/6e47b3a5de6584bc5d5bb2cac7315838/", // 105
-		"https://rutube.ru/video/31a041c7e1d2a2d6ca555a6f7bddae83/", // 106
-		"https://rutube.ru/video/f44c9378fdce5d731d49cfd4cca0720a/", // 107
-		"https://rutube.ru/video/0626231f358970ed1f931909b7d88e85/", // 108
-		"https://rutube.ru/video/dc6ba1d4be7bc4be04e3f02cba202457/", // 109
+		"https://rutube.ru/video/8c27fc8eee3bbece059b6b7e71847b37/", // 110
+		"https://rutube.ru/video/96cb1ed1a1d1542b79e380bdf70f8abb/", // 111
+		"https://rutube.ru/video/1c20721c30a63f6a692a08198f3cf0eb/", // 112
+		"https://rutube.ru/video/1d9ae4f291967abf68f0819a0f59a303/", // 113
+		"https://rutube.ru/video/a73bf5078c5e9be31ae35f1bd7618e09/", // 114
+		"https://rutube.ru/video/20e28616723bae668dee5a3b1654f0b1/", // 115
+		"https://rutube.ru/video/2a2a73d84d6f23b0092c50f9e7c061ae/", // 116
+		"https://rutube.ru/video/fe5a407e8511133a7d525ec7298dc6fd/", // 117
+		"https://rutube.ru/video/3d9ca743ee18a11ffe6a0d7ee0cf21e8/", // 118
+		"https://rutube.ru/video/0e59b63c3b815cbf406fb728a006bc0a/", // 119
+		"https://rutube.ru/video/04b5a735cb673a0529e2b459a8f342ec/", // 120
+		"https://rutube.ru/video/76047edaee63a9a7135e3c2701f80baf/", // 121
+		"https://rutube.ru/video/03b0e878e2ac1fc384a440d764f4241d/", // 122
 	}
 
 	for _, file := range urls {
