d96eaa96b0850a912eafb6333398500c01344785
Stanislav Kharytonov <stanislav@noia.network>
2024-10-11 10:28:40 +0300

    feat: ability to add custom download dir

--- cmd/rutube-dl/main.go
+++ cmd/rutube-dl/main.go
@@ -11,6 +11,7 @@ import (
 func main() {
 	listID := flag.String("list_id", "", "ID of the list to download")
 	fileLink := flag.String("file_link", "", "Direct URL to the file to download")
+	dir := flag.String("dir", "", "directory to store files and temporary chunks, default is - downloads")
 
 	flag.Parse()
 
@@ -23,7 +24,7 @@ func main() {
 		}
 		for _, file := range list {
 			log.Printf("processing: %s\n", file.Title)
-			err := rutubedl.DownloadFile(file.VideoURL, "dwl", 4)
+			err := rutubedl.DownloadFile(file.VideoURL, dir, 4)
 			if err != nil {
 				fmt.Println(err)
 				continue
@@ -31,7 +32,7 @@ func main() {
 		}
 	} else if *fileLink != "" {
 		log.Printf("Downloading file from URL: %s\n", *fileLink)
-		err := rutubedl.DownloadFile(*fileLink, "dwl", 5) // Using 5 workers as an example
+		err := rutubedl.DownloadFile(*fileLink, dir, 5) // Using 5 workers as an example
 		if err != nil {
 			log.Fatalf("failed to download file: %v", err)
 		}
