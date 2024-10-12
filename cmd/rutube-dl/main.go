df4adbbf9cb9352917b479d0922ff8c20821f1a2
Stanislav Kharytonov <stanislav@noia.network>
2024-10-12 22:51:11 +0300

    feat: add workers and ffmpeg usage options for command line

--- cmd/rutube-dl/main.go
+++ cmd/rutube-dl/main.go
@@ -12,6 +12,8 @@ func main() {
 	listID := flag.String("list_id", "", "ID of the list to download")
 	fileLink := flag.String("file_link", "", "Direct URL to the file to download")
 	dir := flag.String("dir", "", "directory to store files and temporary chunks, default is - downloads")
+	workers := flag.Int("workers", 1, "number of workers for chunk download; be careful when using more than 5")
+	withFfmpeg := flag.Bool("with_ffmpeg", false, "use external ffmpeg for more reliable chunk concatenation")
 
 	flag.Parse()
 
@@ -24,7 +26,7 @@ func main() {
 		}
 		for _, file := range list {
 			log.Printf("processing: %s\n", file.Title)
-			err := rutubedl.DownloadFile(file.VideoURL, dir, 4, false)
+			err := rutubedl.DownloadFile(file.VideoURL, dir, *workers, *withFfmpeg)
 			if err != nil {
 				fmt.Println(err)
 				continue
@@ -32,15 +34,17 @@ func main() {
 		}
 	} else if *fileLink != "" {
 		log.Printf("Downloading file from URL: %s\n", *fileLink)
-		err := rutubedl.DownloadFile(*fileLink, dir, 4, false)
+		err := rutubedl.DownloadFile(*fileLink, dir, *workers, *withFfmpeg)
 		if err != nil {
 			log.Fatalf("failed to download file: %v", err)
 		}
 	} else {
 		fmt.Println("Usage: Provide either a -list_id or a -file_link option")
 		fmt.Println("Example:")
-		fmt.Println("  - To download a list: go run main.go -list_id=<list-id> [-dir=<directory>]")
-		fmt.Println("  - To download a file: go run main.go -file_link=<file-url> [-dir=<directory>]")
+		fmt.Println("  - To download a list: go run main.go -list_id=<list-id> [-dir=<directory>] [-workers=<number>] [-with_ffmpeg]")
+		fmt.Println("  - To download a file: go run main.go -file_link=<file-url> [-dir=<directory>] [-workers=<number>] [-with_ffmpeg]")
 		fmt.Println("\nNote: The -dir option is optional. If not specified, it defaults to 'downloads'.")
+		fmt.Println("The -workers option is optional and defaults to 1. Be cautious when using more than 5.")
+		fmt.Println("The -with_ffmpeg option is optional. If enabled, external ffmpeg is used for more reliable chunk concatenation.")
 	}
 }
